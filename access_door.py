91b1ad74090cd399bcc1d79f4ac4cc784b2c4701
Bagas Udi Sahsangka <udibagas@gmail.com>
2017-11-20 16:52:54 +0700

    max volume

--- access_door.py
+++ access_door.py
@@ -1021,6 +1021,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) > 1 and sys.argv[1] == "run":
         mixer.init()
+        mixer.music.set_volume(10)
         logger.debug("Starting GUI...")
         app = QtGui.QApplication(sys.argv)
         ui = Main()
